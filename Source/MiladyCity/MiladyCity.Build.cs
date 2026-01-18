387725a758c09cdb6775b07fa6958b98575b3180
David Fernandez <dfpgrm@Drip-MacBook-Pro.local>
2026-01-18 15:16:12 -0800

    Initial commit - Third Person template

---
+++ Source/MiladyCity/MiladyCity.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MiladyCity : ModuleRules
+{
+	public MiladyCity(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"MiladyCity",
+			"MiladyCity/Variant_Platforming",
+			"MiladyCity/Variant_Platforming/Animation",
+			"MiladyCity/Variant_Combat",
+			"MiladyCity/Variant_Combat/AI",
+			"MiladyCity/Variant_Combat/Animation",
+			"MiladyCity/Variant_Combat/Gameplay",
+			"MiladyCity/Variant_Combat/Interfaces",
+			"MiladyCity/Variant_Combat/UI",
+			"MiladyCity/Variant_SideScrolling",
+			"MiladyCity/Variant_SideScrolling/AI",
+			"MiladyCity/Variant_SideScrolling/Gameplay",
+			"MiladyCity/Variant_SideScrolling/Interfaces",
+			"MiladyCity/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/MiladyCity/MiladyCity.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MiladyCity.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MiladyCity, "MiladyCity" );
+
+DEFINE_LOG_CATEGORY(LogMiladyCity)
